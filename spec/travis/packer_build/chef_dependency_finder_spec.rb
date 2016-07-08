c9759543eef9e09dae07147eaf9d1a4c12be88c5
Dan Buch <dan@travis-ci.org>
2016-07-07 20:48:45 -0400

    Add specs for chef dependency finder

---
+++ spec/travis/packer_build/chef_dependency_finder_spec.rb
@@ -0,0 +1,47 @@
+require 'travis/packer_build'
+
+describe Travis::PackerBuild::ChefDependencyFinder do
+  subject do
+    described_class.new(fake_run_list, [fake_cookbook_path_entry])
+  end
+
+  let(:fake_run_list) { %w(recipe[tofurkey::hotpot) }
+  let(:fake_cookbook_path_entry) { double('cookbook_path_entry') }
+  let(:tofurkey_recipes_default) { double('tofurkey_recipes_default') }
+  let(:tofurkey_recipes_unicorn) { double('tofurkey_recipes_unicorn') }
+  let :tofurkey_recipes do
+    [
+      tofurkey_recipes_default,
+      tofurkey_recipes_unicorn
+    ]
+  end
+
+  before :each do
+    allow(fake_cookbook_path_entry).to receive(:files)
+      .with(%r{.+/tofurkey/recipes/[^/]+\.rb})
+      .and_return(tofurkey_recipes)
+    allow(tofurkey_recipes_default).to receive(:show)
+      .and_return(<<-EOF.gsub(/^\s+> ?/, ''))
+        > include_recipe 'beancurd::firm'
+        > include_recipe 'imagination'
+      EOF
+    allow(tofurkey_recipes_unicorn).to receive(:show)
+      .and_return(<<-EOF.gsub(/^\s+> ?/, ''))
+        > include_recipe 'beancurd::enchanted'
+        >
+        > log('whatebber') { level :hello }
+        >
+        > cookbook_file 'horns.txt' do
+        >   user 'beelzebub'
+        > end
+        >
+        > 1 / 0
+      EOF
+    allow(tofurkey_recipes_unicorn).to receive(:namespaced_path)
+      .and_return('git@nope:ohai.git::cookbooks/tofurkey/recipes/unicorn.rb')
+  end
+
+  it 'finds dependencies' do
+    expect(subject.find).to eql(%w(beancurd imagination tofurkey))
+  end
+end
