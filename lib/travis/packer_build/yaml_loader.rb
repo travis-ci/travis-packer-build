525c3ab5e62beee86c1594c04fa18fac6adcdda4
Dan Buch <dan@travis-ci.org>
2016-07-08 17:38:15 -0400

    Add specs for yaml loader
    and remove unused `.load` method

--- lib/travis/packer_build/yaml_loader.rb
+++ lib/travis/packer_build/yaml_loader.rb
@@ -4,10 +4,6 @@ require 'erb'
 module Travis
   module PackerBuild
     class YamlLoader
-      def self.load(filename)
-        load_string(File.read(filename))
-      end
-
       def self.load_string(string)
         YAML.load(ERB.new(string).result)
       end
