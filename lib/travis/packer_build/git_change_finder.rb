b898b72de9fba2f14c3ea8a6219218e2bf038e73
Dan Buch <dan@travis-ci.org>
2016-07-11 16:46:37 -0400

    Experiment with checking upstream git path changes

--- lib/travis/packer_build/git_change_finder.rb
+++ lib/travis/packer_build/git_change_finder.rb
@@ -7,26 +7,53 @@ module Travis
   module PackerBuild
     class GitChangeFinder
       def initialize(commit_range: %w(@ @), root_repo: '',
-                     clone_tmp: '', git_paths: [], git_logger: nil)
+                     clone_tmp: '', git_paths: [], git_logger: nil,
+                     dig_through_git_paths: false)
         @commit_range = commit_range
         @root_repo = root_repo
         @clone_tmp = clone_tmp
         @git_paths = git_paths
         @git_logger = git_logger
+        @dig_through_git_paths = dig_through_git_paths
       end
 
       def find
-        root_repo_git.gtree(commit_range.first)
-                     .diff(commit_range.last)
-                     .name_status.select { |_, s| %w(M A).include?(s) }
-                     .map do |f, _|
-          Travis::PackerBuild::GitPath.new(root_repo_git, f, commit_range.last)
+        changed_paths = changed_paths_in_range(
+          root_repo_git, commit_range.first, commit_range.last
+        )
+
+        return changed_paths unless dig_through_git_paths
+
+        start_date = root_repo_git.gcommit(commit_range.first).date
+        finish_date = root_repo_git.gcommit(commit_range.last).date
+
+        git_paths.each do |entry|
+          range_start = entry.repo.log(100).since(start_date).last.sha
+          range_finish = entry.repo.log(100).select do |commit|
+            commit.date >= finish_date
+          end.last
+          range_finish = entry.repo.log(1).last.sha if range_finish.nil?
+
+          changed_paths += changed_paths_in_range(
+            entry.repo, range_start, range_finish
+          )
         end
+
+        changed_paths
       end
 
       private
 
       attr_reader :commit_range, :root_repo, :git_paths, :clone_tmp
+      attr_reader :dig_through_git_paths
+
+      def changed_paths_in_range(git, start, finish)
+        git.gtree(start).diff(finish).name_status
+           .select { |_, s| %w(M A).include?(s) }
+           .map do |f, _|
+          Travis::PackerBuild::GitPath.new(root_repo_git, f, finish)
+        end
+      end
 
       def root_repo_git
         Git.bare(root_repo_dir, log: git_logger)
