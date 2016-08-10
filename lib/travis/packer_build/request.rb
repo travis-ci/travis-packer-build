7e51fc46758c7932e40374d9dbbcab6c1098193b
Dan Buch <dan@travis-ci.org>
2016-08-09 23:45:23 -0400

    Add some logging around raw request
    to help clarify what is going on with the various branch settings

--- lib/travis/packer_build/request.rb
+++ lib/travis/packer_build/request.rb
@@ -2,6 +2,11 @@ module Travis
   module PackerBuild
     class Request
       attr_accessor :message, :config, :branch
+
+      def to_s
+        "<#{self.class.name} message=#{message.inspect} " \
+          "config=#{config.inspect} branch=#{branch.inspect}>"
+      end
     end
   end
 end
