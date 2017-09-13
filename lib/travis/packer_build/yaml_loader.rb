6919d582146d7a8650e4c68e7f5a09af2ffa2dbe
Emma Trimble <emma@travis-ci.org>
2017-09-13 18:18:47 -0400

    Add support for "git_desc" when parsing packer templates
    Signed-off-by: Dan Buch <dan@travis-ci.org>

--- lib/travis/packer_build/yaml_loader.rb
+++ lib/travis/packer_build/yaml_loader.rb
@@ -5,7 +5,11 @@ module Travis
   module PackerBuild
     class YamlLoader
       def self.load_string(string)
-        YAML.load(ERB.new(string).result)
+        YAML.load(ERB.new(string).result(binding))
+      end
+
+      def self.git_desc
+        'fafafaf'
       end
     end
   end
